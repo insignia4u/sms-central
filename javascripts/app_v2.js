0cba3d2981a7365526abb84c9134bea968b2b12f
Nicolas Cedron <josenicolascedron@gmail.com>
2014-12-11 22:18:10 -0300

    Build

--- javascripts/app_v2.js
+++ javascripts/app_v2.js
@@ -1,15 +1,24 @@
 (function() {
-  var mySwiper;
-
-  mySwiper = new Swiper('.swiper-container', {
-    scrollContainer: true,
-    mousewheelControl: true,
-    mode: 'horizontal',
-    scrollbar: {
-      container: '.swiper-scrollbar',
-      hide: false,
-      draggable: true
-    }
+  $(document).on('ready', function() {
+    return window.mySwiper = new Swiper('.swiper-container', {
+      scrollContainer: true,
+      mousewheelControl: true,
+      mode: 'horizontal',
+      simulateTouch: true,
+      resistance: '100%',
+      onSwiperCreated: function() {
+        window.swiperCreated = true;
+        if (($('.scroll-container').length)) {
+          return $('.main-column').before($('.tablesaw-bar.mode-columntoggle'));
+        }
+      },
+      scrollbar: {
+        container: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+        simulateTouch: true
+      }
+    });
   });
 
 }).call(this);
@@ -59,17 +68,59 @@
 }).call(this);
 
 (function() {
-  var scrollTable;
+  var showOption;
 
-  scrollTable = new Swiper('#round-corner', {
-    scrollContainer: true,
-    mousewheelControl: true,
-    mode: 'horizontal',
-    scrollbar: {
-      container: '#round-corner-scrollbar',
-      hide: false,
-      draggable: true
-    }
+  this.listItems = [];
+
+  showOption = function() {
+    var itemToShow;
+    itemToShow = $(this).data('show');
+    $.each(listItems, function(index, val) {
+      return $(val).not(itemToShow).hide();
+    });
+    return $(itemToShow).show();
+  };
+
+  $(document).on('ready', function() {
+    $("[data-show]").each(function() {
+      var item;
+      item = $(this).data('show');
+      return listItems.push(item);
+    });
+    $.each(listItems, function(index, val) {
+      return $(val).hide();
+    });
+    $(listItems[0]).show();
+    return $('.js-show-options input').on('click', showOption);
   });
+
+}).call(this);
+
+(function() {
+  $(document).on('ready', function() {
+    $('.js-btn-skip').on('click', function() {
+      var parent;
+      parent = $(this).closest('.column');
+      $('.control-group-box', parent).addClass('s-hidden');
+      return $('.js-box-skip', parent).removeClass('s-hidden');
+    });
+    $('.js-btn-save').on('click', function() {
+      var parent;
+      parent = $(this).closest('.column');
+      $('.control-group-box', parent).addClass('s-hidden');
+      return $('.js-box-save', parent).removeClass('s-hidden');
+    });
+    return $('.js-btn-edit').on('click', function() {
+      var parent;
+      parent = $(this).closest('.column');
+      $('.control-group-box', parent).addClass('s-hidden');
+      return $('.js-box-edit', parent).removeClass('s-hidden');
+    });
+  });
+
+}).call(this);
+
+(function() {
+
 
 }).call(this);
